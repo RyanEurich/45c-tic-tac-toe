b492fc5b4251fb989b1b1ec1704288ca759b3fce
Ryan Eurich <reurich@uci.edu>
2022-10-04 03:38:26 -0700

    added files

---
+++ Functions.cpp
@@ -0,0 +1,26 @@
+#include "Functions.h"
+
+void runGame()
+{
+}
+
+void initBoard(char board[3][3])
+{
+}
+
+void placeAPiece(int x, int y, char piece, char board[3][3])
+{
+}
+
+bool checkForWinner(char piece, char board[3][3])
+{
+}
+
+bool checkForDraw(char board[3][3])
+{
+}
+
+
+void displayBoard(char board[3][3])
+{
+}
